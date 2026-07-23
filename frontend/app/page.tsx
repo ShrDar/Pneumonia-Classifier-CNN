2af1eb1681d1e0d11f065b2397c4f4ade069c950
DAR0987 <darshanshrestha619@gmail.com>
2026-07-23 22:42:07 +0545

    Added: Next JS installed for frontend of the project

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <h1 className="text-4xl">Pneumonia Dectection CNN Model (Efficient Net 18)</h1>
+    </div>
+  );
+}
